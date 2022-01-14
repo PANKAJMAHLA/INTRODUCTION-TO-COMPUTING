a359912f53845b8714c615c1ac1657d9e46926c1
PANKAJMAHLA <97743911+PANKAJMAHLA@users.noreply.github.com>
2022-01-14 19:45:06 +0530

    Add files via upload

---
+++ Assignment 1 Output.py
@@ -0,0 +1,65 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============================================== RESTART: C:\Users\Sahil\Desktop\assignment 1.py ==============================================
+             Program 1
+
+Enter number: 22
+Enter number: 43
+Enter number: 32
+
+Average of the numbers is:  32.33
+
+             Program 2
+___INCOME TAX___
+• All taxpayers are charged a flat tax rate of 20%.
+• All taxpayers are allowed a $10,000 standard deduction.
+• For each dependent, a taxpayer is allowed an additional $3,000 deduction.
+• Gross income must be entered to the nearest penny.
+
+
+Enter your income to the nearest penny(without commas): $3000
+Enter the number of dependents in family: 2000
+You don't have to pay income tax.
+Thanks for using our service.
+
+             Program 3
+
+Enter the number of students in class: 2
+
+Student ID: 21105004
+Student's Name: pankaj kumar
+Student's Gender
+[Note: Use Gender values: ‘F’, ‘M’, ‘U’ (For Unknown).]: M
+Student's course: ECE
+Student's CGPA: 9
+
+Student ID: 21106545
+Student's Name: yash kumar
+Student's Gender
+[Note: Use Gender values: ‘F’, ‘M’, ‘U’ (For Unknown).]: M
+Student's course: ECE
+Student's CGPA: 9
+
+
+Student List:-
+ [[21105004, 'pankaj kumar', 'M', 'ECE', 9.0], [21106545, 'yash kumar', 'M', 'ECE', 9.0]]
+
+             Program 4
+
+Student marks: 90
+Student marks: 80
+Student marks: 72
+Student marks: 64
+Student marks: 62
+
+Sorted List Of Student marks:-
+['62', '64', '72', '80', '90']
+
+             Program 5
+
+a.
+ ['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+b.
+ ['Red', 'Green', 'White', 'Purple', 'Yellow']
