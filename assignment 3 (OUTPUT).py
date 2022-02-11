e9201453f16fa7c7db4166adfe56e90d74f308c7
PANKAJMAHLA <97743911+PANKAJMAHLA@users.noreply.github.com>
2022-02-11 22:09:50 +0530

    Add files via upload

---
+++ assignment 3 (OUTPUT).py
@@ -0,0 +1,111 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Sahil\AppData\Local\Programs\Python\Python310\assignment 3 (INPUT).py
+
+Q.1
+Enter the string:Hello my name is Pankaj Kumar and my branch is ECE 
+
+Dictionary containing {'Word':'Word Count'} is shown below:
+{'pankaj': 1, 'ece': 1, 'kumar': 1, 'my': 2, 'is': 2, 'hello': 1, 'and': 1, 'name': 1, 'branch': 1}
+
+Q.2
+Enter Date below to get date of next day.
+
+Enter Day [1-31]:31
+Enter Month [1-12]:3
+Enter Year [1800-2025]:2004
+
+Next Date in format Day/Month/Year is: 1/4/2004
+
+Q.3
+Enter the numbers separated by space:2 4 6 8 
+
+List containing (n,n^2) is shown below:
+[(2, 4), (4, 16), (6, 36), (8, 64)]
+
+Q.4
+Enter grade points:9
+Your grade is 'A' and Excellent performance.
+
+Q.5
+The pattern is printed below:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+Q.6
+Enter student name:Pankaj Kumar 
+Enter student SID:21105004
+Enter Y to continue or N to end:Y
+Enter student name:Yojit
+Enter student SID:21105009
+Enter Y to continue or N to end:Y
+Enter student name:Rohan
+Enter student SID:21105087
+Enter Y to continue or N to end:Y
+Enter student name:Lucky
+Enter student SID:21105073
+Enter Y to continue or N to end:Y
+Enter student name:Shubham
+Enter student SID:21105034
+Enter Y to continue or N to end:N
+
+Q.6(a)
+The Dictionary containing {'SID':'Name'} is shown below
+{21105004: 'Pankaj Kumar ', 21105009: 'Yojit', 21105087: 'Rohan', 21105073: 'Lucky', 21105034: 'Shubham'}
+
+Q.6(b)
+The Dictionary after sorting according to name:
+{21105073: 'Lucky', 21105004: 'Pankaj Kumar ', 21105087: 'Rohan', 21105034: 'Shubham', 21105009: 'Yojit'}
+
+Q.6(c)
+The Dictionary after sorting according to SID:
+{21105004: 'Pankaj Kumar ', 21105009: 'Yojit', 21105034: 'Shubham', 21105073: 'Lucky', 21105087: 'Rohan'}
+
+Q.6(d)
+Enter SID to get name of student:21105034
+Name of student with SID 21105034 is Shubham.
+
+Q.7
+Enter number of elements N in fibonacci series:
+[N must be positive Integer]: N=13
+
+The fibonacci series with 13 elements is shown below:
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+
+Average of given fibonacci series is 46.85
+
+Q.8
+Set1= {1, 2, 3, 4, 5}
+Set2= {8, 2, 4, 6}
+Set3= {1, 17, 5, 9, 13}
+
+Q.8(a)
+
+A new Set of all 'elements that are in Set1 and Set2 but not both' is:
+{1, 3, 5, 6, 8}
+
+Q.8(b)
+
+A new set of all elements that are 'in only one of the three sets Set1,Set2 and Set3' is:
+{3, 6, 8, 9, 13, 17}
+
+Q.8(c)
+
+A new set of elements that are 'exactly in two of the sets Set1, Set2 and Set3' is:
+{1, 2, 4, 5}
+
+Q.8(d)
+
+A new set of all Integers in the 'range 1 to 10' that are 'not in Set1' is:
+{6, 7, 8, 9, 10}
+
+Q.8(e)
+
+A new set of all Integers in the range 1 to 10 that are not in Set1,Set2 and Set3.
+{10, 7}
