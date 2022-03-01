15706dd427f13c8e52365601a0ed88a94f363b71
PANKAJMAHLA <97743911+PANKAJMAHLA@users.noreply.github.com>
2022-03-01 22:22:15 +0530

    Add files via upload

---
+++ Assignment 4 (OUTPUT).py
@@ -0,0 +1,99 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Sahil\AppData\Local\Programs\Python\Python310\Assignment 4 (INPUT).py
+Answer 1
+Enter number of disks in tower of Hanoi:5
+The Disks are numbered starting from top of the tower. 
+      Steps to move all disks from Source Tower to Destination Tower is given below
+Move disk 1 fromSource TowertoDestination
+Move disk 2 from Source Tower Auxiliary
+Move disk 1 fromDestinationtoAuxiliary
+Move disk 3 from Source Tower Destination
+Move disk 1 fromAuxiliarytoSource Tower
+Move disk 2 from Auxiliary Destination
+Move disk 1 fromSource TowertoDestination
+Move disk 4 from Source Tower Auxiliary
+Move disk 1 fromDestinationtoAuxiliary
+Move disk 2 from Destination Source Tower
+Move disk 1 fromAuxiliarytoSource Tower
+Move disk 3 from Destination Auxiliary
+Move disk 1 fromSource TowertoDestination
+Move disk 2 from Source Tower Auxiliary
+Move disk 1 fromDestinationtoAuxiliary
+Move disk 5 from Source Tower Destination
+Move disk 1 fromAuxiliarytoSource Tower
+Move disk 2 from Auxiliary Destination
+Move disk 1 fromSource TowertoDestination
+Move disk 3 from Auxiliary Source Tower
+Move disk 1 fromDestinationtoAuxiliary
+Move disk 2 from Destination Source Tower
+Move disk 1 fromAuxiliarytoSource Tower
+Move disk 4 from Auxiliary Destination
+Move disk 1 fromSource TowertoDestination
+Move disk 2 from Source Tower Auxiliary
+Move disk 1 fromDestinationtoAuxiliary
+Move disk 3 from Source Tower Destination
+Move disk 1 fromAuxiliarytoSource Tower
+Move disk 2 from Auxiliary Destination
+Move disk 1 fromSource TowertoDestination
+Answer 2
+Enter the number of rows in Pascal's Triangle: 5
+
+By Using recursions:
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+ Alternate method
+
+
+ By Using loops:
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+Answer 3
+
+Enter an Integer:16
+Enter another Integer:5
+
+The quotient when 16 is divided by 5 is 3.
+The remainder when 16 is divided by 5 is 1.
+
+Que3.a
+Function is Not-callable
+
+Que3.b
+All the values are non zero
+
+Que3.c
+Values greater than 4 is 5,6
+
+Que3.d
+Above result in set form is shown below:
+{5, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 6
+Hash value of 6 is 6
+Answer 4
+Hy, My name is Pankaj and my roll no. is 21105004
+Destructor Called
+Answer 5
+a. The Mehak salary is updated to 70000
+b. Employee Viren record deleted
+Answer 6
+
+Enter Word by George:treupmco
+Enter Word by Barbie:computer
+
+Friendship of Barbie and George is REAL
